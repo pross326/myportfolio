4b65860cd12eaf594bf11d9528cdbbe0d7ac0df6
Paul Ross <pross326@gmail.com>
2023-12-30 14:54:27 -0500

    init

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <HeaderVue />
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+import HeaderVue from "./components/HeaderVue.vue";
+
+export default {
+  components: {
+    HeaderVue,
+  },
+};
+</script>
 
 <style>
 #app {
